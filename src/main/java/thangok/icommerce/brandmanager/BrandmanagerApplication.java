d563452f4d073c9e0df2eb92fda1029b3b31dc67
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-02 20:57:35 +0700

    init

---
+++ src/main/java/thangok/icommerce/brandmanager/BrandmanagerApplication.java
@@ -0,0 +1,13 @@
+package thangok.icommerce.brandmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandmanagerApplication.class, args);
+	}
+
+}
