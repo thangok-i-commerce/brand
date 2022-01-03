8a87002dfec41c92300079bd6c7e2867200ecbb2
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-03 21:35:10 +0700

    define brand service

---
+++ src/main/java/thangok/icommerce/brandmanager/service/BrandService.java
@@ -0,0 +1,22 @@
+package thangok.icommerce.brandmanager.service;
+
+import org.springframework.data.domain.Page;
+import thangok.icommerce.brandmanager.dto.BrandDTO;
+
+import java.util.Optional;
+
+public interface BrandService {
+
+    Page<BrandDTO> getAllBrands();
+
+    Page<BrandDTO> searchBrand(String searchText);
+
+    Optional<BrandDTO> getByCode(String brandCode);
+
+    BrandDTO createBrand(BrandDTO newBrand);
+
+    BrandDTO updateBrand(BrandDTO newBrand);
+
+    BrandDTO deleteBrand(String brandCode);
+
+}
---
+++ src/main/java/thangok/icommerce/brandmanager/service/impl/BrandServiceImpl.java
@@ -0,0 +1,42 @@
+package thangok.icommerce.brandmanager.service.impl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import thangok.icommerce.brandmanager.dto.BrandDTO;
+import thangok.icommerce.brandmanager.service.BrandService;
+
+import java.util.Optional;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Override
+    public Page<BrandDTO> getAllBrands() {
+        return null;
+    }
+
+    @Override
+    public Page<BrandDTO> searchBrand(String searchText) {
+        return null;
+    }
+
+    @Override
+    public Optional<BrandDTO> getByCode(String brandCode) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BrandDTO createBrand(BrandDTO newBrand) {
+        return null;
+    }
+
+    @Override
+    public BrandDTO updateBrand(BrandDTO newBrand) {
+        return null;
+    }
+
+    @Override
+    public BrandDTO deleteBrand(String brandCode) {
+        return null;
+    }
+}
