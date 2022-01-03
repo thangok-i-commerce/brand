d8cd3b2d865844e593c91e3191c0beb3f86a4778
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-03 16:22:58 +0700

    disable spring cloud config

--- src/main/java/thangok/icommerce/brandmanager/BrandManagerApplication.java
+++ src/main/java/thangok/icommerce/brandmanager/BrandManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrandmanagerApplication {
+public class BrandManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BrandmanagerApplication.class, args);
+		SpringApplication.run(BrandManagerApplication.class, args);
 	}
 
 }
