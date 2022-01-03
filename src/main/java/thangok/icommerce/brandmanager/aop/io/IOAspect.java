07336ab18f16bad787e66adb91f2d3e978a5cc8e
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-03 23:15:50 +0700

    add IO AOP

--- src/main/java/thangok/icommerce/brandmanager/aop/io/IOAspect.java
+++ src/main/java/thangok/icommerce/brandmanager/aop/io/IOAspect.java
@@ -1,5 +1,6 @@
 package thangok.icommerce.brandmanager.aop.io;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,10 +8,13 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class IOAspect {
     @Around("@annotation(LogIO)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("Enter method [%s] with parameters:(", joinPoint.getSignature()));
         Object[] args = joinPoint.getArgs();
         for (Object arg : args) {
             sb.append(arg).append(",");
@@ -20,7 +24,7 @@ public class IOAspect {
         }
         sb.append(")");
 
-        System.out.println(sb);
+        log.info(sb.toString());
 
         return joinPoint.proceed();
     }
--- src/main/java/thangok/icommerce/brandmanager/dto/BrandDTO.java
+++ src/main/java/thangok/icommerce/brandmanager/dto/BrandDTO.java
@@ -4,6 +4,7 @@ package thangok.icommerce.brandmanager.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class BrandDTO implements Serializable {
 
     private String brandCode;
