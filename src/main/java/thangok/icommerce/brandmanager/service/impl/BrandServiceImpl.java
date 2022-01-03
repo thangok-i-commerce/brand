c4a1fc739e70c27a7911bd830017554f3a7647ff
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-03 22:29:02 +0700

    implement get all brand

--- src/main/java/thangok/icommerce/brandmanager/service/impl/BrandServiceImpl.java
+++ src/main/java/thangok/icommerce/brandmanager/service/impl/BrandServiceImpl.java
@@ -1,18 +1,39 @@
 package thangok.icommerce.brandmanager.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import thangok.icommerce.brandmanager.dto.BrandDTO;
+import thangok.icommerce.brandmanager.entity.Brand;
+import thangok.icommerce.brandmanager.repository.BrandRepository;
 import thangok.icommerce.brandmanager.service.BrandService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BrandServiceImpl implements BrandService {
 
+    @Autowired
+    BrandRepository brandRepository;
+
     @Override
     public Page<BrandDTO> getAllBrands() {
-        return null;
+        List<Brand> brandList = brandRepository.findAll();
+        if (brandList.isEmpty()) {
+            return null;
+        }
+
+        List<BrandDTO> result = new ArrayList<>();
+        brandList.stream().map(x -> new BrandDTO() {{
+            setBrandCode(x.getBrandCode());
+            setBrandName(x.getBrandName());
+            setDescription(x.getDescription());
+        }}).forEach(result::add);
+
+        return new PageImpl<>(result);
     }
 
     @Override
