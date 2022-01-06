9748f84e8cb34d1feb75a68f64086588a5b513e2
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-06 21:54:58 +0700

    declare database

--- src/main/java/thangok/icommerce/brandmanager/entity/Brand.java
+++ src/main/java/thangok/icommerce/brandmanager/entity/Brand.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "brand")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,7 +20,10 @@ public class Brand implements Serializable {
     @Id
     private String brandCode;
 
+    @Column(name = "brand_name")
     private String brandName;
+
+    @Column(name = "description")
     private String description;
 
 }
